d95cd944eb7db19907ab128b59dd0b9b90091910
Shamsafi <118178673+Shamsafi@users.noreply.github.com>
2023-04-02 15:56:38 +0530

    Add files via upload

---
+++ Harmonic_Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Harmonic
+{       //Nth Harmonic Number
+    internal class Harmonic_Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (N == 0)
+            {
+                Console.WriteLine("N cannot be 0.");
+                return;
+            }
+
+            double harmonic = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                harmonic += 1.0 / i;
+            }
+
+            Console.WriteLine($"The {N}th harmonic number is {harmonic}.");
+
+        }
+    }
+}
