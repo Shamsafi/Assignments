acc26ded7d154b9f877cdbb9b82698bbcea40a9e
Shamsafi <118178673+Shamsafi@users.noreply.github.com>
2023-04-02 15:57:50 +0530

    Add files via upload

---
+++ Prime_Program.cs
@@ -0,0 +1,22 @@
+﻿namespace testcase2
+{      //Prime Factor
+    internal class Prime_Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Prime factor of {0}:", n);
+
+            for (int i=2; i<=n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                    n /= i;
+                }
+            }
+        }
+    }
+}
