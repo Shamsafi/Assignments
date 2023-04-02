3a8a836dbcce3bb98f09db5d5df59487fb083d71
Shamsafi <118178673+Shamsafi@users.noreply.github.com>
2023-04-02 15:55:35 +0530

    Add files via upload

---
+++ Leap_Year_Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Leap_Year
+{      //Leap_Year
+    internal class Leap_Year_Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 4-digit year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            bool isLeapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+
+            if (isLeapYear)
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+    }
+}
