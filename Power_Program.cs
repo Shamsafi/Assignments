b97fdb570443aaf83aa51eff124d72154d07c624
Shamsafi <118178673+Shamsafi@users.noreply.github.com>
2023-04-02 15:56:09 +0530

    Add files via upload

---
+++ Power_Program.cs
@@ -0,0 +1,20 @@
+﻿namespace testcase
+{          //Power of 2^N
+    internal class Power_Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 1 || !int.TryParse(args[0], out int n))
+            {
+                Console.WriteLine("Please provide one integer argument N.");
+                
+            }
+
+            for (int i = 7; i <= 20; i++)
+            {
+                Console.WriteLine($"2^{i} = {Math.Pow(2, i)}");
+            }
+        }
+    }
+}
